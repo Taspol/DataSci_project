ff99a5f49e409e7f16a182b5c9cbefc71ac6736a
Taspol <taspolsd@gmail.com>
2024-12-08 02:02:29 +0700

    add ml scatterplot

---
+++ analyze_function/ml_processing.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import time
+import numpy as np
+import pandas as pd
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import networkx as nx
+from collections import Counter
+
+def analyze_category(data):
+    fig = px.scatter(data, x='PCA1', y='PCA2', color='auto_label', 
+                 hover_data={ "dc:title":True},
+                 title="Cluster Visualization with Auto-Labeling",
+                 labels={'auto_label': 'Research Field'},
+                 height=800
+                 )
+
+    return fig
--- pages/3_🤖_ML_Predictions.py
+++ pages/3_🤖_ML_Predictions.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
+from analyze_function.ml_processing import analyze_category
 
-
-st.set_page_config(page_title="ML_Processing", page_icon="🤖")
+st.set_page_config(page_title="ML_Processing", page_icon="🤖",layout='wide')
 
 st.markdown("# ML_Processing 🤖")
 st.sidebar.header("ML_Processing")
@@ -10,6 +10,11 @@ st.write(
     """### ML_Processing: Visualizing Data in Our Web Application
 """
 )
+
+data = pd.read_csv('dataset/k-mean.csv')
+fig1 = analyze_category(data)
+st.plotly_chart(fig1)
+
 
 
 
