5e66821067969bf7205b4a41436fce90a1c6532f
Taspol <taspolsd@gmail.com>
2024-12-10 02:22:21 +0700

    add heatmap

--- pages/2_🌍_Mapping_Demo.py
+++ pages/2_🌍_Mapping_Demo.py
@@ -3,13 +3,15 @@ import pandas as pd
 import pydeck as pdk
 from urllib.error import URLError
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
+st.set_page_config(page_title="Spatial Viz", page_icon="🌍")
 
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
+st.markdown("# Spatial Visualization 🌍")
+st.sidebar.header("Mapping Viz")
 st.write(
-    """This spatial visualize to see colaboration of each country in each paper."""
+    """This spatial visualize represents the relationship in countries column mapping with longtitude and latitude """
 )
+
+# Load the data with caching
 @st.cache_data
 def load_data():
     try:
@@ -20,18 +22,10 @@ def load_data():
         st.error(f"Failed to load data: {e}")
 
 
-# Create a PyDeck line layer to plot the lines between countries
+# Load the data
 line_data,expanded_df =  load_data()
 
-line_layer = pdk.Layer(
-    "PathLayer",
-    line_data,
-    get_path='[[start_lon, start_lat], [end_lon, end_lat]]',
-    get_color=[255, 0, 0],  # Red lines
-    width_scale=10,
-    width_min_pixels=2
-)
-
+# ----------------- PyDeck Map Setup -----------------
 # Create a PyDeck scatter plot layer for the countries (dots on the map)
 scatter_layer = pdk.Layer(
     "ScatterplotLayer",
@@ -41,6 +35,8 @@ scatter_layer = pdk.Layer(
     get_fill_color=[0, 0, 255, 160],  # Blue with some transparency
     pickable=True,
 )
+
+# Create a PyDeck arc layer to show connection between countries
 Outbound_Flow = pdk.Layer(
     "ArcLayer",
     data=line_data,
@@ -57,19 +53,67 @@ Outbound_Flow = pdk.Layer(
 
 # Set up the view for PyDeck
 view_state = pdk.ViewState(
-    latitude=20,  # Center the map
-    longitude=100,
-    zoom=1,
+    latitude=8.199983,  # Center the map
+    longitude=20.998022,
+    zoom=0.5,
     pitch=30
 )
 
 # Create the PyDeck deck with both the line and scatter layers
 deck = pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
-    layers=[    scatter_layer, Outbound_Flow],
+    layers=[scatter_layer, Outbound_Flow],
     initial_view_state=view_state,
     tooltip={"text": "{Country}"}
 )
 
+# ----------------- Setup for map2 -----------------
+# Create a PyDeck scatter plot layer for the countries (dots on the map)
+scatter_layer2 = pdk.Layer(
+    "ScatterplotLayer",
+    expanded_df,
+    get_position='[longitude, latitude]',
+    get_radius=50000,  # Adjust as needed
+    get_fill_color=[0, 0, 255, 160],  # Blue with some transparency
+    pickable=True,
+)
+
+# Create a HeatpMap layer
+heatmap_layer = pdk.Layer(
+    "HeatmapLayer",
+    expanded_df,
+    get_position=['longitude','latitude'],
+    opacity=0.8,
+    pickable=True,
+    radius_pixels=50,
+)
+
+# Set up the view for PyDeck
+view_state2 = pdk.ViewState(
+    latitude=8.199983,  # Center the map
+    longitude=20.998022,
+    zoom=0.5,
+    pitch=0
+)
+
+deck2 = pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    layers=[scatter_layer2,heatmap_layer],
+    initial_view_state=view_state2,
+    tooltip={"text": "{Country}"}
+)
+
+
+# ----------------- Render PyDeck Map -----------------
+st.write(
+    """### Spatial Visualization of Collaboration Between Countries"""
+)
 # Render the PyDeck map in Streamlit
+# chart1
 st.pydeck_chart(deck)
+
+# chart2
+st.write(
+    """### Heatmap of Country that publish the research"""
+)
+st.pydeck_chart(deck2)
